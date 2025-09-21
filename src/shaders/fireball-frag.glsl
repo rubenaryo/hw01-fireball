4faff68c17526c117b1d92811081f9078ad6051f
rubenaryo <rubeny484@gmail.com>
2025-09-20 23:49:03 -0400

    shader cleanup

--- src/shaders/fireball-frag.glsl
+++ src/shaders/fireball-frag.glsl
@@ -1,59 +1,37 @@
 #version 300 es
 
-// This is a fragment shader. If you've opened this file first, please
-// open and read lambert.vert.glsl before reading on.
-// Unlike the vertex shader, the fragment shader actually does compute
-// the shading of geometry. For every pixel in your program's output
-// screen, the fragment shader is run for every bit of geometry that
-// particular pixel overlaps. By implicitly interpolating the position
-// data passed into the fragment shader by the vertex shader, the fragment shader
-// can compute what color to apply to its pixel based on things like vertex
-// position, light position, and vertex color.
 precision highp float;
 
-uniform vec4 u_Color; // The color with which to render this instance of geometry.
 uniform sampler2D u_noiseTexture;
 uniform float u_Time;
 
-// These are the interpolated values out of the rasterizer, so you can't know
-// their specific values without knowing the vertices that contributed to them
 in vec4 fs_Pos;
 in vec4 fs_Nor;
-in vec4 fs_LightVec;
 in vec4 fs_Col;
 
-out vec4 out_Col; // This is the final output color that you will see on your
-                  // screen for the pixel that is currently being processed.
-
+out vec4 out_Col;
 
 // shade a point based on distance
-// Inspired by: https://www.shadertoy.com/view/4ssGzn
+// Inspired by raymarch distance based shading in: https://www.shadertoy.com/view/4ssGzn
 vec4 shadeByDistance(float d)
-{	
-    if (d >= 0.0 && d < 0.2) return (mix(vec4(3, 3, 3, 1), vec4(1, 1, 0, 1), d / 0.2));
-	if (d >= 0.2 && d < 0.5) return (mix(vec4(1, 1, 0, 1), vec4(1, 0, 0, 1), (d - 0.2) / 0.2));
-    return vec4(1.0, 1.0, 1.0, 1.0);
+{
+    float additional = sin(0.5 * u_Time) * 0.05;
+    d += additional;
+
+    vec4 yellow = (mix(vec4(3, 3, 3, 1), vec4(1, 1, 0, 1), d / 0.2));
+    vec4 red = (mix(vec4(1, 1, 0, 1), vec4(1, 0, 0, 1), (d - 0.2) / 0.2));
+    
+    if (d >= 0.0 && d < 0.2) return yellow;
+	if (d >= 0.2 && d < 0.5) return red;
+
+    return mix(vec4(1, 1, 0, 1), vec4(1, 0, 0, 1), smoothstep(0.3, 0.6, d));
 }
 
 void main()
 {
     // fs_Pos is already in local space to the sphere
-    vec4 diffuseColor = shadeByDistance(0.25 * length(fs_Pos));
-
-    float d = length(fs_Pos);
-    //diffuseColor = (mix(vec4(1, 1, 0, 1), vec4(1, 0, 0, 1), (d - 0.2) / 0.2));    
-    diffuseColor.a = 1.0;
-
-    // Calculate the diffuse term for Lambert shading
-    float diffuseTerm = dot(normalize(fs_Nor), normalize(fs_LightVec));
-    // Avoid negative lighting values
-    // diffuseTerm = clamp(diffuseTerm, 0, 1);
-
-    float ambientTerm = 0.2;
-
-    float lightIntensity = diffuseTerm + ambientTerm;   //Add a small float value to the color multiplier
-                                                            //to simulate ambient lighting. This ensures that faces that are not
-                                                            //lit by our point light are not completely black.
+    vec4 diffuseColor = shadeByDistance(0.25 * length(fs_Pos));  
+    diffuseColor.a = 0.85;
 
     // Compute final shaded color
     out_Col = vec4(diffuseColor.rgb, diffuseColor.a);
